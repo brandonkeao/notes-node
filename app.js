21a343ac6ba839e6fc735f7f0383c8972cddd249
Brandon Keao <Brandon.Keao@gmail.com>
2018-09-09 14:08:13 -0600

    refactor for notes funtions

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ console.log('Command:', command);
 console.log('Yargs:', argv);
 
 if(command === 'add') {
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if(note === undefined) {
+    console.log(`Note title already in use.`);
+  } else {
+    console.log(`Note created:`);
+    console.log('--');
+    console.log(`Title: ${argv.title}`);
+    console.log(`Body: ${argv.body}`); 
+  }
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'remove') {
